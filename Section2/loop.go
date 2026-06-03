2b0de303291c6fe86c9d717e9ab14ba4b158e3b5
Sandesh Pulami <sandeshpulami333@gmail.com>
2026-06-03 03:46:41 +0100

    feat: loops

---
+++ Section2/loop.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for - only way to loop
+	// It like C programming loop
+	for i := 1; i <= 10; i++ {
+		//fmt.Println(i)
+	}
+
+	// while loop
+	k := 3
+	for k > 0 {
+		fmt.Println(k)
+		k--
+	}
+}
